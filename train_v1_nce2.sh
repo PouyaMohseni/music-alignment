d8d0717e2766ed03f80e82a212b57bd820774e59
PouyaMohseni <pouyamohseniii@gmail.com>
2026-06-07 12:10:35 -0400

    fix(nce2): reduce time limit to 22h

--- train_v1_nce2.sh
+++ train_v1_nce2.sh
@@ -5,7 +5,7 @@
 #SBATCH --constraint=a100
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --time=22:00:00
 #SBATCH --output=/project/def-ichiro/pmohseni/music-alignment/results/v1_nce2/slurm-%j.log
 #SBATCH --error=/project/def-ichiro/pmohseni/music-alignment/results/v1_nce2/slurm-%j.log
 
